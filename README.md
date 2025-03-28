ca37315a764c9b088ebdfd8781c022140c67bfed
g3310-dev <electro33140@gmail.com>
2025-03-28 15:06:42 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Arunika
+An AI-powered health monitoring app that provides personalized lifestyle recommendations, medication reminders, and online doctor consultations. With accessibility features for people with disabilities, it helps users manage their health anytime, anywhere.
+
+## Team Member:
+- Glenn Wilson Hardian
+- Vinsesius Ferer Banyu Anaro
+
+## Brief Explanation:
+This AI-powered health monitoring application provides users with personalized health management solutions. It offers key features such as AI-based health analysis, personalized lifestyle recommendations, medication reminders, and online doctor consultations via chat or video calls. Additionally, it includes accessibility features for individuals with disabilities, such as object recognition technology for the visually impaired.
+
+## Key Product Features
+- AI-powered health monitoring to analyze user conditions.
+- Personalized healthy lifestyle recommendations, including diet, exercise, and sleep schedules.
+- Customized medication reminders.
+- Online doctor consultations via chat or video calls.
+- Accessibility features for people with disabilities, such as object recognition technology for the visually impaired.
---
+++ lib/Page/get_started.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../Component/button.dart';
+import '../Component/orline.dart';
+import '../Component/text_model.dart';
+
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Align(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  children: [
+                    SizedBox(height: MediaQuery.sizeOf(context).height*0.05),
+                    Row(
+                      children: [
+                        TextWidget(size: 30.0, content: "Arunika", type: 2, colour: 0xFF5285AB, alignment: TextAlign.left)
+                      ],
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: MediaQuery.sizeOf(context).height*0.45,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(image: AssetImage("asset/image/started.png",),
+                            fit: BoxFit.fill,
+                            alignment: Alignment.topCenter,
+                          )
+                      ),
+                    )
+                  ],
+                )
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(left: 27.0, right: 30.0, top: 20.0, bottom: MediaQuery.sizeOf(context).height*0.03),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TextWidget(
+                    size: 30.0,
+                    content: "Selamat datang di",
+                    type: 1,
+                    colour: 0xFF5285AB,
+                    alignment: TextAlign.left,),
+                  const TextWidget(
+                    size: 37.0,
+                    content: "Arunika!",
+                    type: 2,
+                    colour: 0xFF5285AB,
+                    alignment: TextAlign.left,),
+                  const SizedBox(height: 10,),
+                  const TextWidget(
+                      size: 19.0,
+                      content: "Arunika! Bersama Kita Jaga, Rawat, dan Lestarikan Kehidupan untuk Masa Depan yang Lebih Cerah",
+                      type: 1,
+                      colour: 0xFF5285AB,
+                      alignment: TextAlign.left),
+                  SizedBox(height: MediaQuery.sizeOf(context).height*0.05,),
+                  Center(
+                    child: ButtonDef(
+                        width: MediaQuery.sizeOf(context).width*0.9,
+                        height: MediaQuery.sizeOf(context).height*0.04,
+                        type: 1,
+                        onTap: (){
+                          Navigator.pushNamed(context, "/signIn");
+                        },
+                        text: "Yuk Mulai!", size: 15),
+                  ),
+                  const SizedBox(height: 3,),
+                  const OrLine(),
+                  const SizedBox(height: 3,),
+                  Center(
+                    child: ButtonDef(
+                        width: MediaQuery.sizeOf(context).width*0.9,
+                        height: MediaQuery.sizeOf(context).height*0.05,
+                        type: 2,
+                        onTap: (){
+                          Navigator.pushNamed(context, "/signUp");
+                        },
+                        text: "Sudah punya akun", size: 16),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
